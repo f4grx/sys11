1d525c1bb86bd4d681b12e88a0a0cc55cfc4263a
Sebastien F4GRX <f4grx@f4grx.net>
2019-10-18 18:54:11 +0200

    start uart code

--- bootloader/loader.asm
+++ bootloader/loader.asm
@@ -1,12 +1,44 @@
-.text
-.global _start
+	.text
+	.equ REGS, 0x1000
+	.equ BUF,  0x100 /* 256 bytes byffer for command reception */
+	.equ SOF,  0x7E
+	.equ ESC,  0x7D
+	.equ MASK, 0x20
 
+/* UART defs */
+	.set BAUD , 0x2B
+	.set SCCR2, 0x2D
+	.set SCSR , 0x2E
+	.set SCDAT, 0x2F
+
+/* Start of program */
+
+	.global _start
 _start:
-	#prepare indexed access to regs
-	ldx	0x1000
-	#init UART
-_receive:
+	/* prepare indexed access to regs */
+	ldx	#REGS
+	/* init UART */
+	ldaa	#0x30		/* prediv 13, postdiv 1, 9600 bauds @ 8 MHz */
+	staa	BAUD,X
+	ldaa	#0x0C		/* No interrupts, enable TX and RX */
+	staa	SCCR2,X
+
+	/* Wait for character reception */
+_rxsof:
+	brclr	SCSR,X 0x20 _rxsof
+	ldaa	SCDAT,X
+
 	#wait for SOF
+	cmpa	#SOF
+	bne	_rxsof
+
+	#we got a SOF. Now receive up to 256 bytes
+	ldy	#0 /* This is the current buffer length */
+	ldab	#0 /* This is the checksum */
+_rxdata:
+	brclr	SCSR,X 0x20 _rxdata
+	ldaa	SCDAT,X
+
 	#sof received. point to buffer start, init csum
 	#receive chars, update checksum, until eof
 	#eof or overflow
@@ -16,7 +48,6 @@ _receive:
 	#execute command
 	#send response
 	#done
-	jmp _receive
-	#
+	jmp _rxsof
 	rts
 
