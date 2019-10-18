9bdeb5bbd2faaab88d3611f720bf7bc31cca4e5e
Sebastien F4GRX <f4grx@f4grx.net>
2019-10-18 11:34:34 +0200

    more loader code, makefile generate disassembly

--- bootloader/loader.asm
+++ bootloader/loader.asm
@@ -2,5 +2,21 @@
 .global _start
 
 _start:
+	#prepare indexed access to regs
+	ldx	0x1000
+	#init UART
+_receive:
+	#wait for SOF
+	#sof received. point to buffer start, init csum
+	#receive chars, update checksum, until eof
+	#eof or overflow
+	#prepare response
+	#checksum ok?
+	#no: send error code and go back to rx
+	#execute command
+	#send response
+	#done
+	jmp _receive
+	#
 	rts
 
