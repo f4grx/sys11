b4e835418c308bcd8869157a393176d71e8b2bc5
Sebastien F4GRX <f4grx@f4grx.net>
2019-10-21 09:16:18 +0200

    Split uart definitions in an include file add a text file describing memory map

--- bootloader/loader.asm
+++ bootloader/loader.asm
@@ -1,4 +1,14 @@
-	.text
+/* Bootloader program to write arbitrary memory in the hc11 system
+ * (c) 2019 Sebastien Lorquet
+ * Released under WTFPL
+ * Behaviour:
+ * This GNU AS listing assembles to a short binary that is padded to 256
+ * bytes by the linker script.
+ * It can be loaded at address 0x0000 in the hc11 via the bootstrap mechanism.
+ * Once executed it waits for HDLC packets at 9600 bauds on the SCI, and if
+ * checksum (last byte) is valid, it executes the command in the first byte.
+ */
+
 	.equ REGS, 0x1000
 	.equ BUF,  0x100 /* 256 bytes byffer for command reception */
 	.equ SOF,  0x7E
@@ -6,48 +16,43 @@
 	.equ MASK, 0x20
 
 /* UART defs */
-	.set BAUD , 0x2B
-	.set SCCR2, 0x2D
-	.set SCSR , 0x2E
-	.set SCDAT, 0x2F
+	.include "uart.inc"
 
 /* Start of program */
 
+	.text
 	.global _start
 _start:
 	/* prepare indexed access to regs */
 	ldx	#REGS
+
 	/* init UART */
 	ldaa	#0x30		/* prediv 13, postdiv 1, 9600 bauds @ 8 MHz */
 	staa	BAUD,X
 	ldaa	#0x0C		/* No interrupts, enable TX and RX */
 	staa	SCCR2,X
 
-	/* Wait for character reception */
+	/* Wait for reception of SOF (0x7E) */
 _rxsof:
 	brclr	SCSR,X 0x20 _rxsof
-	ldaa	SCDAT,X
+	ldaa	SCDAT,X             /* Got a byte */
+	cmpa	#SOF                /* Is it SOF? */
+	bne     _rxsof              /* No: try agn */
 
-	#wait for SOF
-	cmpa	#SOF
-	bne	_rxsof
-
-	#we got a SOF. Now receive up to 256 bytes
-	ldy	#0 /* This is the current buffer length */
+	/* we got a SOF. Now receive up to 256 bytes */
+	ldy     #0 /* This is the current buffer length */
 	ldab	#0 /* This is the checksum */
+
 _rxdata:
 	brclr	SCSR,X 0x20 _rxdata
-	ldaa	SCDAT,X
+	ldaa	SCDAT,X             /* Got a data byte */
+    
+	/* prepare response */
+	/* checksum ok?
 
-	#sof received. point to buffer start, init csum
-	#receive chars, update checksum, until eof
-	#eof or overflow
-	#prepare response
-	#checksum ok?
-	#no: send error code and go back to rx
-	#execute command
-	#send response
-	#done
-	jmp _rxsof
-	rts
+	/* no: send error code and go back to rx */
+	/* execute command */
+	/* send response */
+	/* done */
+	jmp     _rxsof /* Wait for next command */
 
