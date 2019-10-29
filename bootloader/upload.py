211f0859c67437736beba348d28207d64c016abc
Sebastien F4GRX <f4grx@f4grx.net>
2019-10-29 10:40:35 +0100

    add fast option to benefit from faster /16 divider (5 seconds upload instead of 10). Output cleanup.

--- bootloader/upload.py
+++ bootloader/upload.py
@@ -5,6 +5,8 @@
 import argparse
 import serial
 import os
+import time
+import struct
 
 parser = argparse.ArgumentParser('68hc11 uploader')
 parser.add_argument('binary',
@@ -16,6 +18,9 @@ parser.add_argument('--xtal',
                     default='8',
                     type=float,
                     help='HC11 XTAL frequency in MHz (default %(default)s)')
+parser.add_argument('--fast',
+                    action='store_true',
+                    help='Use the fast /16 baud rate instead of usual /104')
 parser.add_argument('--reset',
                     default='no',
                     choices=['no', 'rts'],
@@ -28,31 +33,38 @@ parser.add_argument('--run',
 args = parser.parse_args()
 args = vars(args)
 
-print(args)
-
 # Compute the baud rate from the xtal clock
 
+if args["fast"] :
+    divider = 16
+else:
+    divider = 13 * 8
+
 eclock    = ((args["xtal"] * 1000000) / 4)
-baudclock = ( eclock / (13*8))
+baudclock = (eclock / divider)
 baud      = baudclock / 16
 
 bauds     = [50,75,110,134,150,200,300,600,1200,1800,2400,4800,9600,19200,38400,
              57600,115200]
-mindist   = 16777216
+minerr   = 1e9
 baud_real = None
 for b in bauds:
-    dist = b - baud
-    if dist < 0: dist = -dist
-    if dist < mindist:
-        mindist = dist
+    err = 100 * (b - baud) / b
+    if err < 0: err = -err
+    if err < minerr:
+        minerr = err
         baud_real = b
+
 if baud_real == None:
     raise "Cannot determine baud rate from xtal clock"
-baud = baud_real
+
+if minerr < 5.0:
+    baud = baud_real
+else:
+    baud = int(baud)
+    print("WARNING this is not a standard baud rate")
 
 # Try opening the program to be bootloaded
-print("Opening bootware: ", args["binary"])
-
 f = open(args["binary"], "rb")
 if f == None:
     raise "Cannot open:"+args[binary]
@@ -67,35 +79,39 @@ f.close()
 
 # Try to open the serial port
 print("Opening port: ", args["port"], "at speed: ", baud)
-
-ser = serial.Serial(args["port"], baud,
-                    bytesize=8, parity='N', stopbits=1, timeout=1)
+ser = serial.Serial(args["port"], baud, timeout=1)
 
 #Optionnally, reset the board
 if args["reset"] == 'rts':
     print("Resetting board via RTS (todo)")
+    ser.rts=False
+    time.sleep(0.1)
+    ser.rts=True
+    time.sleep(0.1)
+    ser.reset_input_buffer() #eat the BREAK that appears as zero byte
+
 
 # All good. Send the initial FF byte
 
 ser.reset_output_buffer()
-ser.write(b'\xff')
+ser.write(b'\xFF')
 
 # Now send the 256 bytes of the program, while checking echos
-
-n = -1
+print("Sending...")
+ser.reset_output_buffer()
+ser.reset_input_buffer()
+n = 0
 for x in binary:
     n = n + 1
-    ser.reset_output_buffer()
-    ser.reset_input_buffer()
-    ser.write(x)
-    echo = ser.read()
+    ser.write(struct.pack("B",x))
+    ser.flush()
+    echo = ser.read(1)
     if len(echo) == 0:
         print("timeout waiting for echo of byte",n)
         continue
     echo = ord(echo)
     if echo != x:
         print("echo error at byte",n,"expected",x,"received",echo)
-
 ser.close()
 
 print("All good. program is running.")
