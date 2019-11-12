8ab85d20f4d0798907303ef6b164767a9f0a6b5a
Sebastien F4GRX <f4grx@f4grx.net>
2019-11-13 00:27:49 +0100

    ram test OK on my board!

--- bootloader/ramtest.asm
+++ bootloader/ramtest.asm
@@ -1,20 +1,13 @@
-/* Bootloader program to write arbitrary memory in the hc11 system
+/* Bootloader program to test external memory in the hc11 system
  * (c) 2019 Sebastien Lorquet
  * Released under WTFPL
  * Behaviour:
  * This GNU AS listing assembles to a short binary that is padded to 256
  * bytes by the linker script.
  * It can be loaded at address 0x0000 in the hc11 via the bootstrap mechanism.
- * Once executed it waits for HDLC packets at 9600 bauds on the SCI, and if
- * checksum (last byte) is valid, it executes the command in the first byte.
  */
 
 	.equ REGS  , 0x1000
-	.equ BUF   , 0x100 /* 256+1 bytes buffer for command reception, in external RAM */
-	.equ BUFEND, 0x201 /* First byte past the buffer */
-	.equ SOF   , 0x7E
-	.equ ESC   , 0x7D
-	.equ MASK  , 0x20
 
 /* HC11 defs */
 	.include "sci.inc"
@@ -29,36 +22,57 @@
 _start:
 /* prepare indexed access to regs */
 	ldx	#REGS
+	cli
 
 /* Enable extended mode */
 	ldaa	HPRIO,X
 	oraa	#HPRIO_MDA		/* Enable extended mode */
-	anda	#~HPRIO_RBOOT		/* Disable bootstrap ROM */
-	anda	#~HPRIO_SMOD		/* Disable special mode */
-	anda	#~HPRIO_IRV
+	anda	#~(HPRIO_RBOOT|HPRIO_SMOD|HPRIO_IRV)
 	staa	HPRIO,X
-
-/* switch on the LED*/
-	ldaa	PACTL,X
-	oraa	#0x80
-	staa	PACTL,X
-	bsr	ledoff		/* ext mem error */
 
 /* Let the bootstrap loader finish sending the last ACK byte */
 waitack:
 	brclr	SCSR,X #SCSR_TC waitack
 
 start_test:
-	ldab	#0x55
-	tba
-	bsr	serhex
-	bsr	serlf
+	ldab	#0x01
+	bsr	testram
+	comb
 	bsr	testram
 
-	ldab	#0xaa
-	tba
-	bsr	serhex
-	bsr	serlf
+	ldab	#0x02
+	bsr	testram
+	comb
+	bsr	testram
+
+	ldab	#0x04
+	bsr	testram
+	comb
+	bsr	testram
+
+	ldab	#0x08
+	bsr	testram
+	comb
+	bsr	testram
+
+	ldab	#0x10
+	bsr	testram
+	comb
+	bsr	testram
+
+	ldab	#0x20
+	bsr	testram
+	comb
+	bsr	testram
+
+	ldab	#0x40
+	bsr	testram
+	comb
+	bsr	testram
+
+	ldab	#0x80
+	bsr	testram
+	comb
 	bsr	testram
 
 	ldaa	#'Z'
@@ -68,6 +82,9 @@ start_test:
 
 
 testram:
+	tba
+	bsr	serhex
+	bsr	serlf
 	ldy	#0x100		/* start test after internal ram */
 fill:
 	stab	0,Y
@@ -77,6 +94,10 @@ fill:
 	ldy	#0x1040
 nextfill:
 	cpy	#0x8000		/* until we end of ram! */
+	bne	nextnextfill
+	ldy	#0xE000
+nextnextfill:
+	cpy	#0xFFFF
 	bne	fill
 
 checkram:
@@ -109,25 +130,11 @@ ok:
 	ldy	#0x1040
 nextcheck:
 	cpy	#0x8000		/* until we end of ram! */
+	bne	nextnextcheck
+	ldy	#0xE000
+nextnextcheck:
+	cpy	#0xFFFF
 	bne	check
-	rts
-
-;------------------------------------------------------------------------------
-; Switches LED ON
-; Destroys A
-ledon:
-	ldaa	PORTA,X
-	anda	#0x7F
-	staa	PORTA,X
-	rts
-
-;------------------------------------------------------------------------------
-; Switches LED OFF
-; Destroys A
-ledoff:
-	ldaa	PORTA,X
-	oraa	#0x80
-	staa	PORTA,X
 	rts
 
 ;------------------------------------------------------------------------------
