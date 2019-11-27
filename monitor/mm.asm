2840de14425c817455bb1be9d67cbc91d3e4ac3c
Sebastien F4GRX <f4grx@f4grx.net>
2019-11-27 17:36:31 +0100

    merge split into alloc, finalize alg (naive)

--- monitor/mm.asm
+++ monitor/mm.asm
@@ -18,46 +18,12 @@ head:	.word	0 /* Pointer to the first free zone */
  */
 	.global	mm_init
 mm_init:
-	ldx	#(RAMLEN-2)	/* Block size = all but two header bytes */
-	stx	RAMSTART
-	ldx	#0xffff
-	stx	RAMSTART+2	/* This is the pointer to next (none) */
 	ldx	#RAMSTART
+	ldd	#(RAMLEN-2)	/* Block size = all but two header bytes */
+	std	0,X
+	ldd	#0xffff
+	std	2,X		/* This is the pointer to next (none) */
 	stx	head		/* Initialize head of free zone list */
-
-	rts
-
-/* Split a free zone in two.
- * After this call the memory now has two linked free zones:
- * one with the requested size (to be allocated immediately)
- * one with the remaining free size.
- * Parameters:
- *   PUSH16 free_zone_pointer
- *   PUSH16 requested_size
- * Return value: None
- * Side effect: the free_zone_ponter now has requested size and
- * a new smaller free zone has been created after the current free zone.
- */
-mm_split:
-	/* TODO check we have at least 2 spare bytes to store the size of the created zone */
-	/* this is an inline routine. sr3 = curblock, sr2 = req size
-	 * sr1 = cur->next
-	 * TODO inline this subroutine
-	 */
-	ldx	sr3	/* after this X contains adr */
-	ldd	sr2	/* after this D contains size */
-	std	0,X	/* POKE adr+SIZE, size */
-	addd	sr3	/* after this D contains size+adr */
-	addd	#2	/* after this D contains size+adr+2 = nxtadr */
-	std	2,X	/* POKE adr+NEXT, nxtadr */
-	xgdx		/* after this X contains nxtadr */
-	ldd	sr1	/* after this D contains PEEK(adr+NEXT) */
-	std	2,X	/* POKE nxtadr+NEXT, PEEK(adr+NEXT) */
-	ldy	sr3	/* after this Y contains adr */
-	ldd	0,Y	/* after this D contains PEEK(adr+SIZE) = freesize */
-	subd	#2	/* after this D contains freesize-2 */
-	subd	sr2	/* after this D contains freesize-2-size = new free size */
-	std	0,X	/* POKE nxtadr+SIZE, PEEK(adr+size)-2-size */
 	rts
 
 /* Allocate a memory zone.
@@ -69,25 +35,46 @@ mm_split:
 mm_alloc:
 	/* Get requested size from stack into sr2 */
 	pulx
-	stx	sr2
+	stx	sr2	/* sr2 <- size */
 
 	/* Browse all zones in the free list.
 	 * We need one that is larger than the requested size
 	 */
 	ldx	head
-	stx	sr3	/* sr3 : pointer to the current zone */
+	stx	sr3	/* sr3 <- adr */
 .Lagain:
-	ldx	sr3
-	ldd	2,X	/* Load the pointer to next zone, preserve X */
-	std	sr1	/* sr1 now contains pointer to next zone */
-	ldx	0,X	/* Load the zone size */
-	stx	sr0	/* sr0 now contains size of free zone */
-	cpx	sr2	/* Compare with required size */
+	ldx	sr3	/* X <- adr */
+	ldx	0,X	/* X <- cursiz */
+	stx	sr0	/* sr0 <- cursiz */
+	cpx	sr2	/* Compare cursiz and size */
 	blo	.Lnext	/* Current free block smaller than request? try next */
 	/* At this point we have a zone that is big enough for allocation */
 	beq	.Lalloc	/* Zone has the correct size */
 	/* Zone is larger than requested, we have to do a split */	
-	bsr	mm_split
+.Lsplit:
+	ldd	sr2	/* D contains size */
+	addd	#2	/* D contains size + 2 */
+	addd	sr3	/* D contains size + 2 + adr == nxtadr */
+	std	sr1	/* sr1 contains nxtadr */
+
+	ldx	sr3	/* X contains adr */
+	ldd	0,X	/* D contains PEEK(adr+SIZE) */
+	subd	#2	/* D contains PEEK(adr+SIZE) - 2 */
+	subd	sr2	/* D contains PEEK(adr+SIZE) - 2 - size == tmp */
+	ldx	sr1	/* X contains nxtadr */
+	std	0,X	/* POKE(nxtadr+SIZE, tmp)*/
+
+	ldx	sr3	/* X contains adr */
+	ldd	2,X	/* D contains PEEK(adr+NEXT) == tmp */
+	ldx	sr1	/* X contains nxtadr */
+	std	2,X	/* POKE(nxtadr+NEXT, tmp) */
+
+	ldx	sr3	/* X contain adr */
+	ldd	sr1	/* D contains nxtadr */
+	std	2,X	/* POKE adr+NEXT, nxtadr */
+	ldd	sr2	/* D contains size */
+	std	0,X	/* POKE adr+SIZE, size */
+
 	/* now the current free zone @sr3 has the correct size */
 
 .Lalloc:
@@ -99,9 +86,8 @@ mm_alloc:
 	ldd	sr1 		/* Get next zone pointer */
 	std	head		/* Update head */
 .Lretblock:
-	inx			/* Make X look at the usable data zone */
-	inx
-	xgdx			/* Store in retval */
+	ldd	sr3
+	addd	#2
 	bra	.Lend		/* We're done! */
 
 .Lnext:	/* Setup pointers to look at next block */
