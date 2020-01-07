b99785983a620db997ac78783c0b7bca6fe97793
Sebastien F4GRX <f4grx@f4grx.net>
2020-01-07 09:54:51 +0100

    add comments

--- monitor/spibus.asm
+++ monitor/spibus.asm
@@ -5,6 +5,24 @@
 	.text
 
 /*===========================================================================*/
+/*
+ * SPI one-time initialization.
+ * - setups PORTD lines
+ * - setups nSS as a normal GPIO under software control
+ */
+	.func	spi_init
+	.global	spi_init
+spi_init:
+	rts
+	.endfunc
+
+/*===========================================================================*/
+/*
+ * Setups the bus for the next transfer
+ * sp0 contains flags to configure bus.
+ * sp1 contains speed divider
+ * word length is fixed to 8 and order is fixed to MSB first
+ */
 	.func	spi_setup
 	.global	spi_setup
 spi_setup:
