b2d2f130bcd393f8d6182e1d02eef553a1ad7b99
Sebastien F4GRX <f4grx@f4grx.net>
2020-01-10 17:59:36 +0100

    add device manager

---
+++ monitor/device.asm
@@ -0,0 +1,72 @@
+/* Storage device manager */
+/* sys11 manage storage devices using numerically indexed devices.
+ * Each device can be implemented by an independent driver.
+ * When calling a generic method of the device manager, the call
+ * is dispatched to the correct device driver.
+ *
+ * IMPORTANT: Block size is fixed at 256 bytes !
+ * This is OK for ROM/RAM and EEPROM based devices.
+ *
+ * devices is an array of pointers to a table of information that MUST
+ * hold the following pointers:
+ * offset description
+ * 0      pointer to a device type name string
+ * 2      pointer to function that returns the block count
+ * 4      pointer to function that reads a block
+ * 6      pointer to function that writes a block
+ */
+
+	.include "softregs.inc"
+
+	.section .scommands
+	.asciz	"devlist"
+	.word	devlist
+
+	.equ	NUM_DEVS, 8	/* Max number of devices */
+
+	.section edata
+devices:
+	.space NUM_DEVS * 2
+
+	.text
+
+/*===========================================================================*/
+/* Initialize the device layer */
+	.func	device_init
+	.global device_init
+device_init:
+	rts
+	.endfunc
+
+/*===========================================================================*/
+/* shell command to report the list of devices */
+	.func	devlist
+	.global devlist
+devlist:
+	rts
+	.endfunc
+
+/*===========================================================================*/
+/* Returns in D the num of blocks for device in sp0 */
+	.func	device_getblocks
+	.global device_getblocks
+device_getblocks:
+	rts
+	.endfunc
+
+/*===========================================================================*/
+/* Returns in buffer at sp1 the contents of block sp2 of device sp0 */
+	.func	device_readb
+	.global device_readb
+device_readb:
+	rts
+	.endfunc
+
+/*===========================================================================*/
+/* Write contents at sp1 in block sp2 of device sp0 */
+	.func	device_writeb
+	.global device_writeb
+device_writeb:
+	rts
+	.endfunc
+
