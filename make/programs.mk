73a1006812dc49ade9ece0dd3e769b1ac9be3b44
Sebastien F4GRX <f4grx@f4grx.net>
2020-04-06 11:09:44 +0200

    many more words

--- make/programs.mk
+++ make/programs.mk
@@ -2,7 +2,7 @@ default: $(TARGETS)
 
 define BUILD_TARGET
 .PHONY: $(1)
-$(1): $(1).bin $(1).srec $(1).txt
+$(1): $(1).bin $(1).srec $(1).dis
 	@size $(1).elf
 
 $(1).elf: $$($(1)_OBJS) $$($(1)_LKR)
@@ -14,11 +14,11 @@ $(1).bin: $(1).elf
 $(1).srec: $(1).elf
 	m68hc11-elf-objcopy -j .text -j .rodata -j .vectors -O srec $(1).elf $(1).srec
 
-$(1).txt: $(1).elf
-	m68hc11-elf-objdump -ds $(1).elf > $(1).txt
+$(1).dis: $(1).elf
+	m68hc11-elf-objdump -ds $(1).elf > $(1).dis
 
 $(1)_clean:
-	rm -f $(1).bin $(1).srec $(1).elf $(1).txt $(1).map $($(1)_OBJS)
+	rm -f $(1).bin $(1).srec $(1).elf $(1).dis $(1).map $($(1)_OBJS)
 endef
 
 $(foreach TARGET, $(TARGETS), $(eval $(call BUILD_TARGET,$(TARGET))))
