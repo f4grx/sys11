d157c9ac67b5443fd31b2db2603f63732680b884
Sebastien F4GRX <f4grx@f4grx.net>
2020-05-13 17:21:28 +0100

    avoid edges to return

--- tools/forthdic.py
+++ tools/forthdic.py
@@ -209,7 +209,7 @@ while ptr < off+length:
         print("[%04X] " % (ptr+base), end='')
         wordname = findsymname(word).decode()
         print("    %04X" % word, wordname)
-        if dot and not(prevname=='BRANCH' or prevname=='BRANCHZ' or prevname=='JNZD'):
+        if dot and not(prevname=='BRANCH' or prevname=='BRANCHZ' or prevname=='JNZD') and wordname!='RETURN':
             edge = "\"%s\" -> \"%s\""%(internalname,wordname)
             gr[edge] = True
         ptr += 2
