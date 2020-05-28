049c9bcd37405c6cea8bc1673bbeed207804f87e
Sebastien F4GRX <f4grx@f4grx.net>
2020-05-28 11:33:07 +0200

    delete sim from this repo, moved to another one (see clone script)

---
+++ git-clone.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+#this script checks out the external repos that were part of this repo.
+# THESE ARE NOT SUBMODULES!
+git clone https://github.com/f4grx/hc11-sim sim
+
--- sim/sci.h
+++
@@ -1,9 +0,0 @@
-#ifndef __sci__h__
-#define __sci__h__
-
-struct hc11_sci;
-
-struct hc11_sci* hc11_sci_init(struct hc11_core *core);
-void hc11_sci_close(struct hc11_sci *sci);
-
-#endif /* __sci__h__ */
